18c85071842216bd57150312ba727096109a397a
KImSangGeon <tkdrjs7@naver.com>
2021-05-21 14:43:02 +0900

    @bean validation 전까지 기본 세팅

---
+++ src/main/java/spring5_webmvc_bean_validation_study/config/ControllerConfig.java
@@ -0,0 +1,10 @@
+package spring5_webmvc_bean_validation_study.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"spring5_webmvc_bean_validation_study.controller"})
+public class ControllerConfig {
+
+}
---
+++ src/main/java/spring5_webmvc_bean_validation_study/controller/RegisterController.java
@@ -0,0 +1,53 @@
+package spring5_webmvc_bean_validation_study.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import spring5_webmvc_bean_validation_study.spring.DuplicateMemberException;
+import spring5_webmvc_bean_validation_study.spring.MemberRegisterService;
+import spring5_webmvc_bean_validation_study.spring.RegisterRequest;
+
+
+@Controller
+public class RegisterController {
+		@Autowired
+		private MemberRegisterService service;
+
+		@RequestMapping("/register/step1")
+		public String handleStep1() {
+			return "register/step1";
+		}
+		@PostMapping("/register/step2")
+		public String handleStep2(@RequestParam(value = "agree", defaultValue = "false" ) 
+																Boolean agree, /*Model model , */RegisterRequest registerRequest) {
+			if(!agree) {
+				return "register/step1";
+			}				//이메일이 같아서 되돌아와도 값이 남아있는 이유는 model로 만들어놔서,
+//			model.addAttribute("registerRequest", new RegisterRequest());
+			return "register/step2";
+		}
+		
+		@GetMapping("/register/step2")
+		public String handleStep2Get() {
+			return "redirect:/register/step1";
+		}
+		@PostMapping("/register/step3")
+		public String handleStep3(/* @ModelAttribute("formData") */ RegisterRequest reqReq, Errors errors) {
+			if(errors.hasErrors())
+				return "register/step2";
+		
+			try {
+				service.regist(reqReq);
+				return "register/step3";
+			}catch (DuplicateMemberException ex) {
+				errors.rejectValue("email", "duplicate");
+				return "register/step2";
+			}
+			
+		}
+}
